87757aee83a16587f5c649142336e7da4522c8b4
brucedominguez <bruce@platformcrossfit.com.au>
2019-01-09 20:35:07 +0800

    second commit

---
+++ scripts/exampleChoc.ps1
@@ -0,0 +1,7 @@
+# Install Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Globally Auto confirm every action
+choco feature enable -n allowGlobalConfirmation
+
+# Install Python 2.x 2.7.15
+choco install python2
